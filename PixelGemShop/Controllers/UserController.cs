dee2ada76824223f153fa1636f2b383625757497
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-04-11 17:02:51 +0200

    User profile html

---
+++ PixelGemShop/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using PixelGemShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PixelGemShop.Controllers
+{
+    public class UserController : Controller
+    {
+        private DBContext db = new DBContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            var currentUser = int.Parse(User.Identity.Name);
+            var user = db.Users.Where(u => u.IdUser == currentUser).FirstOrDefault();
+            return View(user);
+        }
+
+        public ActionResult OrderHistory()
+        {
+            var currentUser = int.Parse(User.Identity.Name);
+            var orders = db.Orders.Where(o => o.IdUser == currentUser).Include(o => o.OrderDetails);
+            return View(orders.ToList());
+        }
+    }
+}
