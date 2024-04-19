ce4e9b8c7c7a759ce0aeec50a3ff1d6493072e9a
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-04-19 18:51:58 +0200

    censor pass in user's profile

--- PixelGemShop/Models/Users.cs
+++ PixelGemShop/Models/Users.cs
@@ -25,10 +25,12 @@ namespace PixelGemShop.Models
 
         [Required]
         [StringLength(20)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [StringLength(50)]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
