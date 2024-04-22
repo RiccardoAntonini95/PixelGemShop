5b50e88a100b6d0dd9ed2cf6b81012fa9c3d931d
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-04-22 16:59:52 +0200

    avg stars in home and categories

---
+++ PixelGemShop/Scripts/avgStars.js
@@ -0,0 +1,20 @@
+﻿var avgRatingInput = document.querySelectorAll('.average-rating');
+var starContainers = document.querySelectorAll('.stars-container');
+
+var numStars = [];
+
+// Calcola il numero di stelle piene arrotondate per ogni input di media
+for (let i = 0; i < avgRatingInput.length; i++) {
+    var value = avgRatingInput[i].value.replace(',', '.');
+    numStars[i] = Math.round(parseFloat(value));
+}
+
+// Itera su ogni container di stelle e colora le stelle corrispondenti
+starContainers.forEach(function (container, index) {
+    var stars = container.querySelectorAll('.star-avg-rating');
+
+    // Colora le prime numStars stelle nel container corrente
+    for (var i = 0; i < numStars[index]; i++) {
+        stars[i].querySelector('path').setAttribute('fill', 'gold');
+    }
+});
