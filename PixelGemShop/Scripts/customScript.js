c203ca800b18a530f7e82d7a36b023cc973f0e42
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-05-22 16:47:39 +0200

    Todo: dark/light

--- PixelGemShop/Scripts/customScript.js
+++ PixelGemShop/Scripts/customScript.js
@@ -1,4 +1,6 @@
 ﻿const containerLoader = document.querySelector(".loader-container");
+const lightModeBtn = document.querySelector("#lightModeBtn");
+const body = document.body;
 
 window.addEventListener("load", () => {
     containerLoader.classList.add("hidden")
@@ -9,3 +11,8 @@ var alertDiv = document.getElementById('alertDiv');
 setTimeout(function () {
     alertDiv.style.display = 'none'; 
 }, 3000); 
+
+lightModeBtn.addEventListener("click", () => {
+    console.log("clic")
+    body.style.color = "red"
+})
